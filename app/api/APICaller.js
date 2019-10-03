61e05537f8fb46e2e4e5761e8ff0ff4eb00b1da3
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-10-03 18:53:11 +0700

    add api caller

---
+++ app/api/APICaller.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { getToken } from "./Session";
+
+const baseUrl = API_BASE_DOMAIN;
+
+export default function Caller(
+  endpoint,
+  method = "GET",
+  { params = {}, ...body } = {}
+) {
+  return axios(`${baseUrl}/api/${endpoint}`, {
+    method: method,
+    data: body,
+    params: params,
+    withCredentials: true,
+    headers: {
+      ...(getToken() ? { Authorization: `Bearer ${getToken()}` } : {})
+    }
+  });
+}
